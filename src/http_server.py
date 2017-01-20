1b3a13ec2233c068c877d788a00af58df5642a2a
colinhmit <colinh@mit.edu>
2017-01-19 21:53:53 -0500

    [cph] removed requesting logging & changed gif

--- src/http_server.py
+++ src/http_server.py
@@ -126,7 +126,6 @@ class StreamClient():
         request = {}
         request[src] = {'add':[stream]}
 
-        pp('Requesting stream: '+ stream)
         self.request_sock.send(json.dumps(request))
 
     def get_agg_streams(self, args):
@@ -144,7 +143,7 @@ class StreamClient():
         if ('twitter' in args) and (len(args['twitter'][0])>0):
             for stream_id in [self.pattern.sub('',x).lower() for x in args['twitter'][0].split(',')]:
                 if stream_id not in self.twitter_streams:
-                    self.twitter_streams[stream_id] = {("This stream is not currently available. If this message does not dissapear, please try one of the following streams: " + str(self.target_twitter_streams)): {"mp4_url": "", "score": 0.0001, "first_rcv_time": "2001-01-01T00:00:00.000000", "media_url": "https://media.giphy.com/media/qNQFUVIKAt9Ek/giphy.gif"}}
+                    self.twitter_streams[stream_id] = {("This stream is not currently available. If this message does not dissapear, please try one of the following streams: " + str(self.target_twitter_streams)): {"mp4_url": "", "score": 0.0001, "first_rcv_time": "2001-01-01T00:00:00.000000", "media_url": "https://media.giphy.com/media/a9xhxAxaqOfQs/giphy.gif"}}
                     self.request_stream(stream_id,'twitter')
 
                 trend_dicts.append(self.twitter_streams[stream_id])
