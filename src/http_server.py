e256badab3132e644c3bdca0d8d16a253c91ebff
colinhmit <colinh@mit.edu>
2017-01-19 22:07:21 -0500

    [cph] missing ). this is why you test locally first boys

--- src/http_server.py
+++ src/http_server.py
@@ -145,8 +145,7 @@ class StreamClient():
                 if stream_id not in self.twitter_streams:
                     self.twitter_streams[stream_id] = {("This stream is not currently available. If this message does not dissapear, please try one of the following streams: " + str(self.target_twitter_streams)): {"mp4_url": "", "score": 0.0001, "first_rcv_time": "2001-01-01T00:00:00.000000", "media_url": "https://media.giphy.com/media/a9xhxAxaqOfQs/giphy.gif"}}
                     self.request_stream(stream_id,'twitter')
-                
-                trend_dicts.append(self.twitter_streams.get(stream_id,{("This stream is not currently available. If this message does not dissapear, please try one of the following streams: " + str(self.target_twitter_streams)): {"mp4_url": "", "score": 0.0001, "first_rcv_time": "2001-01-01T00:00:00.000000", "media_url": "https://media.giphy.com/media/a9xhxAxaqOfQs/giphy.gif"}})
+                trend_dicts.append(self.twitter_streams.get(stream_id,{("This stream is not currently available. If this message does not dissapear, please try one of the following streams: " + str(self.target_twitter_streams)): {"mp4_url": "", "score": 0.0001, "first_rcv_time": "2001-01-01T00:00:00.000000", "media_url": "https://media.giphy.com/media/a9xhxAxaqOfQs/giphy.gif"}}))
         
         output = {}
         [output.update(d) for d in trend_dicts]
