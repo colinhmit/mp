67efcb4797e6ecb0343c23e88e66b53fb32d0f48
colinhmit <colinh@mit.edu>
2017-03-20 18:57:59 -0400

    [cph] turned off manual/reddit/dataserver for prod

--- src/stream_server.py
+++ src/stream_server.py
@@ -30,12 +30,12 @@ class StreamServer():
 
         self.input_server = InputServer(inputconfig, self.config['init_streams'])
 
-        self.native_manager = NativeManager(streamconfig)
+        #self.native_manager = NativeManager(streamconfig)
         self.twitch_manager = TwitchManager(streamconfig, self.input_server.irc, self.config['init_streams']['twitch'])
         self.twitter_manager = TwitterManager(streamconfig, self.input_server.twtr, self.config['init_streams']['twitter'])
-        self.reddit_manager = RedditManager(streamconfig, self.input_server.rddt, self.config['init_streams']['reddit'])
+        #self.reddit_manager = RedditManager(streamconfig, self.input_server.rddt, self.config['init_streams']['reddit'])
         
-        self.data_server = DataServer(dataconfig)
+        #self.data_server = DataServer(dataconfig)
 
         self.init_sockets()
         self.init_threads()
