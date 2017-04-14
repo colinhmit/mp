fd9d064d12cac6c80660716ca26e2ca20c17b70c
colinhmit <colinh@mit.edu>
2017-04-14 17:45:59 -0400

    [cph] 200->''

--- src/inputs/utils/mnl.py
+++ src/inputs/utils/mnl.py
@@ -25,7 +25,7 @@ class mnlWebServer(Resource):
     def render_POST(self, data):
         input_msg = data.content.getvalue()
         self.manual_server.handle_msg(input_msg)
-        return 200
+        return ''
 
 class mnl:
     def __init__(self, config):
