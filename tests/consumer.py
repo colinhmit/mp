155def636641cd4a3213eb05746ffa802ac49a85
colinhmit <colinh@mit.edu>
2017-05-18 20:33:11 -0400

    [cph] pushing server

--- tests/consumer.py
+++ tests/consumer.py
@@ -3,17 +3,11 @@ import zmq
 import random
 
 def consumer():
-    consumer_id = random.randrange(1,10005)
-    print "I am consumer #%s" % (consumer_id)
     context = zmq.Context()
     # recieve work
-    consumer_receiver = context.socket(zmq.PULL)
-    consumer_receiver.connect("tcp://127.0.0.1:5557")
-
-    for work in iter(consumer_receiver.recv_string, 'STOP'):
-        data = int(work)
-        result = { 'consumer' : consumer_id, 'num' : int(data)}
-        if data%2 == 0: 
-            print result
+    consumer_receiver = context.socket(zmq.SUB)
+    consumer_receiver.bind("tcp://127.0.0.1:8000")
+    for work in iter(consumer_receiver.recv, 'STOP'):
+        print work
 
 consumer()
