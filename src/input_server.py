9fd93da61313d420f227d4700f9e19d45ebce342
colinhmit <colinh@mit.edu>
2017-06-10 17:30:48 -0400

    [cph] turning off twitter & twitch

--- src/input_server.py
+++ src/input_server.py
@@ -27,12 +27,12 @@ class InputServer:
         self.nlp_parser = nlpParser()
 
         self.ntv = ntv(self.config['ntv_config'])
-        self.irc = irc(self.config['irc_config'], init_streams['twitch'])
-        self.twtr = twtr(self.config['twtr_config'], init_streams['twitter'])
+        #self.irc = irc(self.config['irc_config'], init_streams['twitch'])
+        #self.twtr = twtr(self.config['twtr_config'], init_streams['twitter'])
         #self.rddt = rddt(self.config['rddt_config'], init_streams['reddit'])
         self.mnl = mnl(self.config['mnl_config'])
 
         self.native_input = NativeInput(self.config['ntv_config'], self.nlp_parser)
-        self.twitch_input = TwitchInput(self.config['irc_config'], self.nlp_parser)
-        self.twitter_input = TwitterInput(self.config['twtr_config'], self.nlp_parser)
+        #self.twitch_input = TwitchInput(self.config['irc_config'], self.nlp_parser)
+        #self.twitter_input = TwitterInput(self.config['twtr_config'], self.nlp_parser)
         #self.reddit_input = RedditInput(self.config['rddt_config'], self.nlp_parser)
